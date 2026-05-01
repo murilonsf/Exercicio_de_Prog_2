129c4ad5959fb8f897ce9becb896794417bbd362
gabrielhaick <gabrielcha@al.insper.edu.br>
2026-05-01 18:38:46 -0300

    Calcula pontos na regra avançada - Quadra v4

--- funcoes.py
+++ funcoes.py
@@ -83,7 +83,7 @@ def calcula_pontos_quadra(dadosrolados):
         else:
             verificador[dado] = 1
     numeros = list(verificador.values())
-    if 4 in valores:
+    if 4 in numeros:
         soma = 0
         for dado in dadosrolados:
             soma += dado
